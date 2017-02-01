a0bcf6b69f454ef3c8e65f8922a5a1d69d70f938
Ariel Caba <mcarielcaba@gmail.com>
2017-02-01 11:31:32 +0800

    update string for undefined name

--- api/controllers/hello.js
+++ api/controllers/hello.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var util = require('util');
+const util = require('util');
 
 module.exports = {
   hello: hello
@@ -8,7 +8,7 @@ module.exports = {
 
 function hello(req, res, next) {
   // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
-  let name = req.swagger.params.name.value || 'stranger';
+  let name = req.swagger.params.name.value || 'Buddy';
   let hello = util.format('Hello, %s!', name);
 
   res.status(200).json({
